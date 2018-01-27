91576860369a02929134a0ce2e2a7520720b08a5
Tyler Stanish <tystanish@gmail.com>
2018-01-27 15:03:56 -0500

    added redux and ran into some problems so adding experimental branch for now

--- App.js
+++ App.js
@@ -1,30 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import iTunes from 'react-native-itunes';
-
-export default class App extends React.Component {
-
-  componentDidMount(){
-    console.log('got here');
-    iTunes.getTracks().then(tracks => console.log(tracks)).catch(e => console.log(e));
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import {Text} from 'react-native';
+import {} from 'redux';
+export default () => <Text>Bla</Text>;
