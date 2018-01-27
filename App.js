2b4f396fad2d58a86b94686c4f3d27562b2960fa
Tyler Stanish <tystanish@gmail.com>
2018-01-27 15:28:08 -0500

    added typescript

--- App.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {} from 'redux';
-export default () => <Text>Bla</Text>;
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default class extends React.Component{
+  render(){
+    return(
+      <Text>Bla bla bla bla bla bla bla bla</Text>
+    );
+  }
+}
